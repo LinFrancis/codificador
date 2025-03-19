7cca72b789d882e6bd58cb23679fbdeb6c072088
Lin Francis <64030176+LinFrancis@users.noreply.github.com>
2025-03-19 16:21:15 -0300

    Add files via upload

--- codificador.py
+++ codificador.py
@@ -82,12 +82,13 @@ st.markdown(
 # Título y descripción (con contenedor de degradado)
 st.markdown("""
 <div class="title-container">
-    <h1 class="title-text">Codificador 3000 Dra. Javiera Saavedra Nazer</h1>
+    <h1 class="title-text">Codificador 3000 - Dra. Javiera Saavedra Nazer</h1>
 </div>
 """, unsafe_allow_html=True)
 
 st.markdown("""
-Bienvenido al Codificador 3000–  
+Bienvenido al Codificador 3000
+ 
 Utiliza la barra lateral para filtrar las entradas por **fuente** mediante el menú desplegable y utiliza el campo de búsqueda avanzado a continuación para ingresar uno o varios términos (usa "AND" u "OR") y encontrar rápidamente textos o títulos que contengan esos términos.
 
 **[LinkedIn de la Dra. Javiersa Saavedra Nazer](https://www.linkedin.com/in/javiera-saavedra-nazer-md-faadv-582a7448/)**
